1504d0b9f98bb438c0d345eec0ce9d86b93e4068
zachibs <zachibsn@gmail.com>
2023-01-07 23:27:28 +0200

    finish part a

--- src/main/java/com/Ex2_1.java
+++ src/main/java/com/Ex2_1.java
@@ -6,8 +6,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Hello world!
@@ -28,7 +33,7 @@ public class Ex2_1
                 File myNewFile = new File(newFileName);
                 if (myNewFile.createNewFile()) {
                     fileNames[i-1] = newFileName;
-                    System.out.println("File created: " + newFileName);
+                    // System.out.println("File created: " + newFileName);
                     Path filePath = Path.of(newFileName);
                     int numberOfLines = randomObject.nextInt(bound);
                     for (int j = 0; j < numberOfLines; j++){
@@ -69,13 +74,85 @@ public class Ex2_1
         return numberOfLines;
     }
 
-    public int getNumOfLinesThreads(String[] fileNames){
+    
+    public static int getNumOfLinesThreads(String[] fileNames){
+        List<NumReaderThread> threadList = new ArrayList();
+        int sumOfLines = 0;
         
-        return 0;
+        for(int i = 0; i < fileNames.length; i++){
+            String name = fileNames[i];
+            NumReaderThread myNewThread = new NumReaderThread(name);
+            threadList.add(myNewThread);
+        }
+        
+        for(int i = 0; i < fileNames.length; i++){
+            threadList.get(i).start();
+        }
+
+        for(int i = 0; i < fileNames.length; i++){
+            try{
+                threadList.get(i).join();
+            } catch (InterruptedException e){
+                System.out.println(e);
+            }
+            sumOfLines += threadList.get(i).getNumOfLines();
+        }
+
+        return sumOfLines;
+    }
+
+    public static int getNumOfLinesThreadPool(String[] fileNames){
+        int sumOfLines = 0;
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(fileNames.length);
+        Future<Integer> futureList[] = new Future[fileNames.length];
+
+        for(int i = 0; i < fileNames.length; i++){
+            String name = fileNames[i];
+            NumReaderCallable myNewThread = new NumReaderCallable(name);
+            Future<Integer> intResult = threadPool.submit(myNewThread);
+            futureList[i] = intResult;
+        }
+
+        for(int i = 0; i < fileNames.length; i++){
+            try{
+                sumOfLines += futureList[i].get();
+            } catch (InterruptedException e){
+                System.out.println(e);
+            } catch(ExecutionException e){
+                System.out.println(e);
+            }
+        }
+
+        threadPool.shutdown();
+        
+        return sumOfLines;
+    }
+
+    public static double differenceTimeMili(double firstTimeMili){
+        double lastTimeMili = System.currentTimeMillis();
+
+        return (lastTimeMili - firstTimeMili) / 1000;
     }
 
     public static void main( String[] args ){
-        String[] fileList = createTextFiles(3, 2, 30);
-        System.out.println(getNumOfLines(fileList));
+        String[] fileList = createTextFiles(4000, 2, 100);
+
+        double firstTimeMili = System.currentTimeMillis();
+        System.out.print("Regular counting: ");
+        System.out.print(getNumOfLines(fileList));
+        System.out.print("\nTime took: " + differenceTimeMili(firstTimeMili));
+        
+        firstTimeMili = System.currentTimeMillis();
+        System.out.print("\nThread counting: ");
+        System.out.print(getNumOfLinesThreads(fileList)); 
+        System.out.print("\nTime took: " + differenceTimeMili(firstTimeMili));
+        
+        firstTimeMili = System.currentTimeMillis();
+        System.out.print("\nThreadPool counting: ");
+        System.out.print(getNumOfLinesThreadPool(fileList));      
+        System.out.print("\nTime took: " + differenceTimeMili(firstTimeMili));
+        
+            
     }
 }
---
+++ src/main/java/com/NumReaderCallable.java
@@ -0,0 +1,35 @@
+package com;
+
+import java.util.concurrent.Callable;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class NumReaderCallable implements Callable {
+
+    String fileName;
+    int numOfLines;
+
+    public NumReaderCallable(String fileName){
+        this.fileName = fileName;
+        this.numOfLines = 0;
+    }
+
+    @Override
+    public Object call() throws Exception {
+        List<String> lines;
+        Path filePath = Paths.get(this.fileName);
+        try{
+            lines = Files.readAllLines(filePath);
+            this.numOfLines += lines.size();
+        } catch (IOException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return this.numOfLines;
+    }
+    
+}
